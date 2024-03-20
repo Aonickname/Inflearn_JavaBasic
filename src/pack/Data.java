0530168189bddfb0bdf92f29f5b375ee4865f756
sebins <bysws302@naver.com>
2024-03-20 19:36:19 +0900

    패키지 - 시작

---
+++ src/pack/Data.java
@@ -0,0 +1,8 @@
+package pack;
+
+public class Data {
+    public Data() {
+        System.out.println("패키지 pack Data 생성");
+        pack.a.User user = new pack.a.User();
+    } 
+}
---
+++ src/pack/packageMain1.java
@@ -0,0 +1,8 @@
+package pack;
+
+public class packageMain1 {
+    public static void main(String[] args) {
+        Data data = new Data();
+
+    }
+}
