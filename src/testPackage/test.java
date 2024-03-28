495b7d1865052239527f56fe61535fac8ae17b66
sebins <bysws302@naver.com>
2024-03-28 21:28:27 +0900

    PC, Notebook git test

--- src/testPackage/test.java
+++ src/testPackage/test.java
@@ -2,6 +2,9 @@ package testPackage;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("test");
+
+        System.out.println("NoteBook test");
+        System.out.println("PC test");
     }
+
 }
